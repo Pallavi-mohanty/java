eba70a6afb01416a7ac87a04c295d82b8743b087
Pallavi-mohanty/java <pallavimohantypallu31@gmail.com>
2022-08-21 11:43:40 +0530

    exercise

---
+++ Exercise_2.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+public class Exercise_2 {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		  System.out.println("Enter the size of the array:");
+		  int i,temp=0,size;
+		     size=sc.nextInt();
+		     int arr[ ]=new int[size];
+		  System.out.println("Enter the Element of the array:");
+		      for(i=0;i<size;i++)
+		     {
+		         arr[i]=sc.nextInt();
+		     }
+		      int ele;
+		      System.out.print("\nEnter the search element:");
+		      ele=sc.nextInt();
+		      
+		      System.out.print("\nArray elements are:");
+		      for(i=0;i<size;i++)
+		      {
+		       System.out.print(arr[i]+" ");
+		      }
+		      int index = 0;
+		      for(i=0;i<size;i++)
+		      {
+		          if(arr[i]==ele)
+		          {
+		              temp=1;
+		              index=i;
+		          }
+		      }
+		      if(temp==1)
+		    	  System.out.print("\nIndex of Search Element "+ele+" is "+index);
+		      else
+		    	  System.out.print("\nElement is not found....");
+		     sc.close();
+		 }
+		}
